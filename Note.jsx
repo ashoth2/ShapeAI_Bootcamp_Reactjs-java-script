2279063ad34d87aa81a03d5219af016080efcac5
ashoth2 <85402642+ashoth2@users.noreply.github.com>
2021-06-05 20:04:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3>As construction pvt.ltd.</h3>
+      <h1>Customer Name: Vasu</h1>
+      <p> Buying products</p>
+      <li> Bricks</li>
+      <li>Raw metirial</li>
+      <li>Cement</li>
+      <li>Wood</li>
+    </div>
+  );
+}
+export default Note;
